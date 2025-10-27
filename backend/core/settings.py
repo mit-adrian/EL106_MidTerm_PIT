61265e31760d13cb0d1051579b821871987a9fdd
appdevadrian <appdev.adrian@gmail.com>
2025-10-27 12:12:47 +0800

    add video folder in static_github, add 1 image too and update README

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #App
+    # Third-party apps
+    'rest_framework',
+
+    #App custom apps
     'core',
 ]
 
